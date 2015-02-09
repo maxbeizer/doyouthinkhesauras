5839f43ae8284844d0b7df4182bdb71e9ffa34c0
Max Beizer <max.beizer@gmail.com>
2015-02-08 22:12:03 -0600

    Add progress on moving images

--- Doyouthinkhesauras/ViewController.swift
+++ Doyouthinkhesauras/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var positionSlider: UISlider!
+    @IBOutlet weak var grant: UIImageView!
+    @IBOutlet weak var trex: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,5 +25,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func silderValueChange(sender: UISlider) {
+        let duration = 2.0
+        let delay = 0.0
+        let options = UIViewKeyframeAnimationOptions.CalculationModeLinear
+        UIView.animateKeyframesWithDuration(duration, delay: delay, options: options, animations: {
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 3/3, animations: {
+                self.grant.transform = CGAffineTransformMakeTranslation(CGFloat(self.positionSlider.value * 100), CGFloat(0))
+                self.trex.transform = CGAffineTransformMakeTranslation(CGFloat(self.positionSlider.value * 275), CGFloat(0))
+            })
+        }, completion: {finished in
+            // do something?
+        })
+    }
 }
 
